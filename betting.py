04dada72a65b10f94d6917a27a7397ab907bb890
Paul Pedro <sablos@hotmail.com>
2020-04-23 08:54:42 -0400

    Input validation to prevent ValueError crashing

--- betting.py
+++ betting.py
@@ -1,17 +1,25 @@
 import random
 
 again = "y"
+limit = 3
 # score tracking variables
 wins = losses = 0
 
 while again == "y":
-    win_number = random.randint(1, 3)
+    win_number = random.randint(1, limit)
     # skip a line here
     print()
-    guess = int(input("Guess the lucky number (between 1 and 3): "))
+    valid = False
+    while not valid:
+        guess = input(f"Guess the lucky number (between 1 and {limit}): ")
+        if guess.isnumeric():
+            num_guess = int(guess)
+            # we can never get a value error here because we know guess is numeric
+            if num_guess in range(1, limit + 1):
+                valid = True
     print(f"The lucky number is {win_number}")
-    if guess == win_number:
-        print("you won!")
+    if win_number == num_guess:
+        print("You won!")
         wins += 1
     else:
         print("You lost!")
