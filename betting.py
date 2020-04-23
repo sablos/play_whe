5400041ae7ae39f33da124a97eb4a2f7efbffa73
Paul Pedro <sablos@hotmail.com>
2020-04-22 22:45:03 -0400

    added if logic, tells you whether you won or lost

--- betting.py
+++ betting.py
@@ -4,3 +4,7 @@ win_number = random.randint(1, 3)
 
 guess = int(input("Guess the lucky number (between 1 and 3): "))
 print(f"The lucky number is {win_number}")
+if guess == win_number:
+    print("you won!")
+else:
+    print("You lost!")
