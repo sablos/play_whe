031b22e3e859e47e07b613297a634f1f51fa0a1c
Paul Pedro <sablos@hotmail.com>
2020-04-23 07:04:32 -0400

    Slight formatting improvement with some blank lines

--- betting.py
+++ betting.py
@@ -1,10 +1,13 @@
 import random
 
 again = "y"
+# score tracking variables
 wins = losses = 0
 
 while again == "y":
     win_number = random.randint(1, 3)
+    # skip a line here
+    print()
     guess = int(input("Guess the lucky number (between 1 and 3): "))
     print(f"The lucky number is {win_number}")
     if guess == win_number:
@@ -14,4 +17,6 @@ while again == "y":
         print("You lost!")
         losses += 1
     print(f"Score: wins {wins}, losses {losses}")
+    # skip line
+    print()
     again = input("Play again? (y/n): ")
