9476505d2897b71bb82265f2b0d27bc68a80c009
Paul Pedro <sablos@hotmail.com>
2020-04-22 22:56:35 -0400

    game repeats in loop

--- betting.py
+++ betting.py
@@ -1,10 +1,13 @@
 import random
 
-win_number = random.randint(1, 3)
+again = "y"
 
-guess = int(input("Guess the lucky number (between 1 and 3): "))
-print(f"The lucky number is {win_number}")
-if guess == win_number:
-    print("you won!")
-else:
-    print("You lost!")
+while again == "y":
+    win_number = random.randint(1, 3)
+    guess = int(input("Guess the lucky number (between 1 and 3): "))
+    print(f"The lucky number is {win_number}")
+    if guess == win_number:
+        print("you won!")
+    else:
+        print("You lost!")
+    again = input("Play again? (y/n): ")
