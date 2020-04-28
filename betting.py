27153858361accdfa9614489518124f5d8d0049f
Paul Pedro <sablos@hotmail.com>
2020-04-28 09:38:30 -0400

    Replaced validatoin code with a function

--- betting.py
+++ betting.py
@@ -5,6 +5,27 @@ limit = 10
 # score tracking variables
 score = 0
 
+
+def get_valid_number():
+    valid = False
+    while not valid:
+        guess = input(f"Guess the lucky number (between 1 and {limit}): ")
+        if guess.isnumeric() and int(guess) in range(1, limit + 1):
+            valid = True
+    return int(guess)
+
+
+def get_valid_input():
+    guess = input(f"Guess the lucky number (between 1 and {limit}): ")
+    valid = False
+    while not valid:
+        if guess.isnumeric() and int(guess) in range(1, limit + 1):
+            valid = True
+        else:
+            guess = input(f"Your guess must be between 1 and {limit}: ")
+    return int(guess)
+
+
 # main game loop
 while again == "y":
     # generate random winning number
@@ -13,13 +34,7 @@ while again == "y":
     print()
 
     # ask user for first guess
-    valid = False
-    while not valid:
-        guess = input(f"Guess the lucky number (between 1 and {limit}): ")
-        if guess.isnumeric():
-            num_guess = int(guess)
-            if num_guess in range(1, limit + 1):
-                valid = True
+    num_guess = get_valid_number()
 
     if win_number == num_guess:
         print("Awesome! You nailed it on the first try!")
@@ -31,13 +46,8 @@ while again == "y":
 
     # give the player a second chance if they didn't get the first guess
     if num_guess != win_number:
-        valid = False
-        while not valid:
-            guess = input(f"Guess again: ")
-            if guess.isnumeric():
-                num_guess = int(guess)
-                if num_guess in range(1, limit + 1):
-                    valid = True
+        num_guess = get_valid_input()
+
         if num_guess == win_number:
             score += 3
             print("You got it! Good job!")
@@ -46,15 +56,10 @@ while again == "y":
         else:
             print("That's still not it. Guess a bit higher.")
 
+    # third and final chance
     if num_guess != win_number:
-        valid = False
-        while not valid:
-            guess = input(f"Last guess: ")
-            if guess.isnumeric():
-                num_guess = int(guess)
-                # we can never get a value error here because we know guess is numeric
-                if num_guess in range(1, limit + 1):
-                    valid = True
+        num_guess = get_valid_input()
+
         if num_guess == win_number:
             score += 1
             print("Finally you got it right!")
