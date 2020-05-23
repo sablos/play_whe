3fdd6b21db173f6ef4ad5164f2606527c759ae68
Paul Pedro <sablos@users.noreply.github.com>
2020-05-23 09:25:13 -0400

    Uses for loop and preset number of rounds. Made the validation function more reusable with max Added accuracy score at the end and introduction at beginning

--- betting.py
+++ betting.py
@@ -1,31 +1,34 @@
 import random
 
-again = "y"
 limit = 10
 # score tracking variables
 score = 0
 
 
-def get_valid_number(prompt):
-    guess = input(prompt)
+def get_valid_number(prompt, max):
+    entry = input(prompt)
     valid = False
     while not valid:
-        if guess.isnumeric() and int(guess) in range(1, limit + 1):
+        if entry.isnumeric() and int(entry) in range(1, max + 1):
             valid = True
         else:
-            guess = input(f"Your guess must be between 1 and {limit}: ")
-    return int(guess)
+            entry = input(f"Your entry must be between 1 and {max}: ")
+    return int(entry)
 
+
+print("Hello! Welcome to \"Guess The Number\"! Test your psychic abilities!")
+rounds = get_valid_number("How many games would you like to play (up to 100)?: ", 100)
 
 # main game loop
-while again == "y":
+for round in range(1, rounds + 1):
     # generate random winning number
     win_number = random.randint(1, limit)
     # skip a line here
     print()
 
+    print(f"Round {round}...")
     # ask user for first guess
-    num_guess = get_valid_number(f"Guess the lucky number (between 1 and {limit}): ")
+    num_guess = get_valid_number(f"Guess the lucky number (between 1 and {limit}): ", limit)
 
     if win_number == num_guess:
         print("Awesome! You nailed it on the first try!")
@@ -37,7 +40,7 @@ while again == "y":
 
     # give the player a second chance if they didn't get the first guess
     if num_guess != win_number:
-        num_guess = get_valid_number("Second chance: ")
+        num_guess = get_valid_number("Second chance: ", limit)
 
         if num_guess == win_number:
             score += 3
@@ -49,7 +52,7 @@ while again == "y":
 
     # third and final chance
     if num_guess != win_number:
-        num_guess = get_valid_number("Final guess: ")
+        num_guess = get_valid_number("Final guess: ", limit)
 
         if num_guess == win_number:
             score += 1
@@ -60,4 +63,6 @@ while again == "y":
     print(f"Score: {score}")
     # skip line
     print()
-    again = input("Play again? (y/n): ")
+
+accuracy = score / (5 * rounds) * 100
+print(f"Great! After {rounds} rounds, your guessing accuracy is {accuracy:.1f}%")
